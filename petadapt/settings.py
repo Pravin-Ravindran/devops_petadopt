715f340b3152b47e3ec69e516ebf268ee76b2b16
EC2 Default User <ec2-user@ip-172-31-14-10.eu-west-1.compute.internal>
2024-04-07 22:26:35 +0000

    Final Commit

--- petadapt/settings.py
+++ petadapt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)q@5z12)+ur)o@fsf%v5-uff&5byb5x*syef)eyvt0ii%*!m#='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
